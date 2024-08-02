bda52b67a1bc99625963172fb04ebe1305285169
shemanto27 <shemantosharkarofficial@gmail.com>
2024-08-03 02:31:26 +0600

    html done

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
- 
+# from flask import Flask, render_template
+# import pymongo
+from hashids import Hashids
+
+# app = Flask(__name__)
+
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+# db = client["URL_Shortener"]
+# collection = db["All_URLs"]
+# collection.insert_one(dictionary)
+
+# def url_shortener(URL):
+generated_hashid = Hashids(salt="hello", min_length=4)
+    # return generated_hashid
+
+print("hello")
+print(generated_hashid)
+
+
+
+
+
+
+# @app.route('/')
+# def home():
+#     return render_template("base.html")
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
